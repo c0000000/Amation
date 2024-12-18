6f12987fb58d843f0ac0f7efdfe3573572f57aa8
Th3os02 <157703336+Th3os02@users.noreply.github.com>
2024-12-18 20:29:25 +0100

    animeAdapter e connesione a json con paginacerca

---
+++ app/src/main/java/it/itsrizzoli/amation/AnimeAdapter.java
@@ -0,0 +1,61 @@
+package it.itsrizzoli.amation;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide; // Assicurati di avere Glide nel tuo build.gradle
+
+import java.util.List;
+
+import it.itsrizzoli.amation.R;
+import it.itsrizzoli.amation.model.AnimeModel;
+
+public class AnimeAdapter extends BaseAdapter {
+    private Context context;
+    private List<AnimeModel> animeList;
+
+    public AnimeAdapter(Context context, List<AnimeModel> animeList) {
+        this.context = context;
+        this.animeList = animeList;
+    }
+
+    @Override
+    public int getCount() {
+        return animeList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return animeList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return animeList.get(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_scheda_anime, parent, false);
+        }
+
+        ImageView imageView = convertView.findViewById(R.id.copertinaImageView);
+        TextView textView = convertView.findViewById(R.id.titoloTextView);
+
+        AnimeModel anime = animeList.get(position);
+        textView.setText(anime.getTitle());
+
+        // Carica l'immagine usando Glide
+        Glide.with(context)
+                .load(anime.getPicture())
+                .into(imageView);
+
+        return convertView;
+    }
+}
